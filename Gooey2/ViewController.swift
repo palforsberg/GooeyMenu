37d165ac33e88bfe10d244d60e93749185b6a4f2
Pål <palforsberg@gmail.com>
2015-02-18 20:32:44 +0100

    Initial Commit

---
+++ Gooey2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Gooey2
+//
+//  Created by Pål Forsberg on 2015-02-18.
+//  Copyright (c) 2015 Pål Forsberg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
